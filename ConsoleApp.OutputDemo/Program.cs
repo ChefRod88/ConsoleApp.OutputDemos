6d80764e2a52a714196ea7d25f1dc37fab3f6f1c
Owner <Owner@DESKTOP-BT3BM35>
2025-03-27 10:46:12 -0400

    Add project files.

---
+++ ConsoleApp.OutputDemo/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World! My name is Trevoir Williams"); //writes on a new line 
+
+Console.WriteLine("I am from a beautiful island from Jamaica");
+
+Console.WriteLine("Anything");
+
+Console.Write("hello1"); // writes on same line 
+Console.Write("hello2");
+Console.Write("hello3");
+
+
+
---
+++ ConsoleApp.VariablesDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿// Diffrent datatypes
+/*
+ text- string
+ integers -int
+ decimal - double, float, decimal
+ logical - bool
+ */
+string name = "Rodney";
+
+Console.WriteLine(name);
+Console.WriteLine("I am "+ name); // string concatenation
+Console.WriteLine($"I am {name}"); // string interpolaton
+Console.WriteLine("I was given the name {0}", name); // formatted string
+
+int age = 36;
+Console.WriteLine("My age is " + age);
+int retirementYearsLeft = 11;
+int retirementAge = age + retirementYearsLeft;
+
+Console.WriteLine("My age is when I retire " + retirementAge);
+
+bool isRetired = true;
+Console.WriteLine("Am I retired? " + isRetired);
