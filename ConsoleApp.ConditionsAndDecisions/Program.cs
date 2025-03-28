740d4e1cef362b75148156c84dfa3d4cbfe945b7
Owner <Owner@DESKTOP-BT3BM35>
2025-03-28 14:05:07 -0400

    Switch Statements

--- ConsoleApp.ConditionsAndDecisions/Program.cs
+++ ConsoleApp.ConditionsAndDecisions/Program.cs
@@ -70,3 +70,65 @@ Console.WriteLine("****************** Tenary Operator Result *****************")
 
 // The rest of the applicatiom
 Console.WriteLine("Thank you for using this program");
+
+//Switch statements - used to evaluate a value and take action 
+Console.WriteLine("****************** Switch Statement Result *****************");
+/*
+ Write a program to accept an integer as the day of the week and prnt the approiate message as outlined below.
+1 - Sunday
+2 - Monday
+3 - Tuesday
+4 - Wednesday (Hump Day)
+5 - Thursday
+6 - Friday (TGIF !!!)
+7 - Saturday (Beach Day!!!)
+ */
+Console.WriteLine("Please enter the day of the week");
+int dayOfTheWeek = Convert.ToInt32(Console.ReadLine());
+
+switch (dayOfTheWeek)
+{
+    case 1:
+        Console.WriteLine("Sunday");
+        break;
+
+    case 2:
+        Console.WriteLine("Monday");
+        break;
+
+    case 3:
+        Console.WriteLine("Tuesday");
+        break;
+
+    case 4:
+        Console.WriteLine("Wednesday (Hump Day)");
+        break;
+
+    case 5:
+        Console.WriteLine("Thursday");
+        break;
+
+    case 6:
+        Console.WriteLine("Friday (TGIF !!!)");
+        break;
+
+    case 7:
+        Console.WriteLine("Saturday (Beach day !!!)");
+        break;
+    default:
+        
+        Console.WriteLine("invalid day number entered");
+        break;
+        
+        
+
+
+}
+
+
+
+Console.WriteLine("****************** Switch Statement Result End *****************");
+
+
+// The rest of the application 
+Console.WriteLine("Thank you for using this program");
