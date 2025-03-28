fe99b2279ec1355148505aef364846bb7de4695e
Owner <Owner@DESKTOP-BT3BM35>
2025-03-28 14:53:01 -0400

    Initial Commit

---
+++ ConsoleApp.SampleCalculator/Program.cs
@@ -0,0 +1,16 @@
+﻿// Welcome Message
+Console.WriteLine("Welcome To The Sample Calculator");
+
+// Prompt for user input 
+Console.WriteLine("Please enter the first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Please enter the second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+// Show calculator options
+
+// Decide which operation is needed based on selected option
+
+// Print output 
+
