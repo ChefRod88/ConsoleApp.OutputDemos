7b1ba725832f02d84483f6fef61cbe476e0e8a4a
Owner <Owner@DESKTOP-BT3BM35>
2025-04-01 20:52:36 -0400

    Functional Calculator

--- ConsoleApp.SampleCalculator/Program.cs
+++ ConsoleApp.SampleCalculator/Program.cs
@@ -1,5 +1,7 @@
 ﻿// Welcome Message
-Console.WriteLine("Welcome To The Sample Calculator");
+using System.ComponentModel;
+
+Console.WriteLine("******************** Welcome To The Sample Calculator  ************************");
 
 // Prompt for user input 
 Console.WriteLine("Please enter the first number: ");
@@ -8,9 +10,61 @@ int num1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Please enter the second number: ");
 int num2 = Convert.ToInt32(Console.ReadLine());
 
-// Show calculator options
+// Show calculator options/ Show Menu
 
-// Decide which operation is needed based on selected option
+Console.WriteLine("Please select an operation.");
+Console.WriteLine("1. Addition");
+Console.WriteLine("2. Subtraction");
+Console.WriteLine("3. Multiplication");
+Console.WriteLine("4. Division");
+int choice = Convert.ToInt32(Console.ReadLine());
 
-// Print output 
+/* Decide which operation is needed based on selected option
+// if statements 
+if (choice == 1 )
+{
+    // addition
+}
+else if (choice == 2 )
+{
+    // subraction
+}
+else if (choice == 3 )
+{
+    // multiplication
+}
+else if (choice == 4 )
+{
+    // division
+}
+else
+{
+    Console.WriteLine("Invalid Choice");
 
+}*/
+int answer = 0;
+switch (choice)
+{
+    case 1: 
+        // addition
+        answer = num1 + num2;
+        break;
+    case 2:
+        // subtraction
+        answer = num1 - num2;
+        break;
+    case 3:
+        // multiplication
+        answer = num1 * num2;
+        break;
+    case 4:
+        // division
+        answer = num1 / num2;
+        break;
+    default:
+        Console.WriteLine("Invalid choice");
+        break;
+
+}
+// print output
+Console.WriteLine($"The result is: {answer}");
