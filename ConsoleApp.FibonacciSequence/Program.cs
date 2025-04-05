a148d0f18e54e4f785764ce206e6205f57e32425
Owner <Owner@DESKTOP-BT3BM35>
2025-04-05 09:11:13 -0400

    This is the Fibonacci Sequence and just to let you know I don't like math but it is tolerable

---
+++ ConsoleApp.FibonacciSequence/Program.cs
@@ -0,0 +1,21 @@
+﻿int n = 25;
+int a = 0;
+int b = 1;
+int sum = 0;
+
+Console.WriteLine("This is the Fibonacci Sequence and just to let you know I don't like math but it is tolerable.");
+Console.WriteLine();
+
+for (int i = 0; i < n; i++)
+{
+    Console.WriteLine($"{a}");
+    sum += a;
+
+    int temproraryVariable = a; 
+
+    a = b;
+
+    b += temproraryVariable;
+}
+Console.WriteLine();
+Console.WriteLine($"The sum of the Fibonacci sequence is {sum}");
