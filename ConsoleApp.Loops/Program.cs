b35767291e42b29f7c5e5299e48f140ed4e64e01
Owner <Owner@DESKTOP-BT3BM35>
2025-04-05 06:58:22 -0400

    HELLO WORLD x5

--- ConsoleApp.Loops/Program.cs
+++ ConsoleApp.Loops/Program.cs
@@ -9,3 +9,13 @@ for (int i = 0; i < 10; i++)
 
 Console.WriteLine("Loop Completed ");
 // Ask User how many times they wish to print hello world and print accordingly
+
+
+Console.WriteLine("How many time do you want to print HELLO WORLD?");
+Console.Write("Enter here:");
+int numberOfTimes = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 0; i < numberOfTimes; i++)
+{
+    Console.WriteLine($"Hello, World {i}"); numberOfTimes
+}
