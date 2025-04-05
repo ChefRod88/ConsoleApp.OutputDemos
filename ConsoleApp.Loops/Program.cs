a1d16caf4b73759594fcb38ff5cc94a0c8d3d959
Owner <Owner@DESKTOP-BT3BM35>
2025-04-05 06:45:34 -0400

    For Loops

---
+++ ConsoleApp.Loops/Program.cs
@@ -0,0 +1,11 @@
+﻿// Simple For Loop Demo
+// Print Hello World 10 times
+
+// 0 1 2 3 4 5 6 7 8 9
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine($"Hello, World {i}");
+}
+
+Console.WriteLine("Loop Completed ");
+// Ask User how many times they wish to print hello world and print accordingly
