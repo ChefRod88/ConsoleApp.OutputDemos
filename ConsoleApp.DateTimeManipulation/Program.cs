bb464c39ecbfa9599cfb849ec07b67536627ea45
Owner <Owner@DESKTOP-BT3BM35>
2025-04-09 09:20:41 -0400

    Intial Commit

---
+++ ConsoleApp.DateTimeManipulation/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("********** - DateTime manipulation - **********");
+
+// Empty DateTime
+
+// Create a DateTime from date and time
+
+// Create a DateTime drom current timestamp
+
+// Create a DateTime from a string
+
+// Add Additional DateTime
+
+// Ticks from DateTime
+Console.WriteLine("********** - DateTime Offset manipulation - **********");
+// UTC
+
+// DateTimeOffset and TimeZone Info
+
+Console.WriteLine("********** - Date only and Time only manipulation - **********");
+
+// DateOnly
+
+// TimeOnly
+
