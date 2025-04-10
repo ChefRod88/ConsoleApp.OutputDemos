39f75de1f3f225ce5bb4da299b188a41a4100461
Owner <Owner@DESKTOP-BT3BM35>
2025-04-10 08:31:32 -0400

    Explore DateTime Manipulations

--- ConsoleApp.DateTimeManipulation/Program.cs
+++ ConsoleApp.DateTimeManipulation/Program.cs
@@ -2,11 +2,48 @@
 
 // Empty DateTime
 
+DateTime dateTime = new DateTime();
+
 // Create a DateTime from date and time
+
+var dateOfBirth = new DateTime(1988,8,16);
+Console.WriteLine($"My DOB is {dateOfBirth}");
+
+var exactDateOfBirth = new DateTime(1988, 8, 16, 14, 45, 45);
+Console.WriteLine($"My DOB is {exactDateOfBirth}");
+
+Console.WriteLine($"Day of the week: {dateOfBirth.DayOfWeek}");
+Console.WriteLine($"Day of the year {dateOfBirth.DayOfYear}");
+Console.WriteLine($"Time of day {exactDateOfBirth.TimeOfDay}");
+Console.WriteLine($"Tick: {exactDateOfBirth.Ticks}");
+Console.WriteLine($"Kind: {exactDateOfBirth.Kind}");
+
+
 
 // Create a DateTime drom current timestamp
 
+DateTime now = DateTime.Now;
+Console.WriteLine("The time now is: " + now);
+
 // Create a DateTime from a string
+Console.WriteLine("What is you DOB (dd/MM/yyyy): ");
+string dob = Console.ReadLine();
+
+var userDob = DateTime.Parse( dob );
+Console.WriteLine($"Day of the week: {userDob.DayOfWeek}");
+Console.WriteLine($"Day of the year {userDob.DayOfYear}");
+Console.WriteLine($"Time of day {userDob.TimeOfDay}");
+Console.WriteLine($"Tick: {userDob.Ticks}");
+Console.WriteLine($"Kind: {userDob.Kind}");
+
+
+// Change Format DateTime 
+Console.WriteLine($"Formatted Date: {userDob.ToString("dd/MM/yyyy")}");
+Console.WriteLine($"Formatted Date: {userDob.ToString("MMM, dd-yyyy")}");
+Console.WriteLine($"Formatted Date: {userDob.ToString("dddd-MMMM-yyyy")}");
+Console.WriteLine($"Formatted Date: {userDob:dddd-MMMM-yyyy}");
+
+
 
 // Add Additional DateTime
 
