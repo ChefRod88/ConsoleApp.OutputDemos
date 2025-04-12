c5c3dfc771ae6c1f42bb782202ef091572cfaa8e
Owner <Owner@DESKTOP-BT3BM35>
2025-04-12 15:38:19 -0400

    Lists

--- ConsoleApp.Lists/Program.cs
+++ ConsoleApp.Lists/Program.cs
@@ -1,9 +1,44 @@
 ﻿Console.WriteLine("********** - LISTS - **********");
 
 // Declare a List
+List<int> grades = new List<int>();
+int grade = 0;
+//var grades = new List<int>();
+//List<int> grades = new();
 
 // Add valuest to a list
 
+//grades.Add(45);
+//grades.Add(95);
+
+//Console.WriteLine(grades[0]);
+//Console.WriteLine(grades[1]);
+
+do
+{
+    Console.WriteLine("Enter Grade: ");
+    grade = Convert.ToInt32(Console.ReadLine());
+
+    if (grade != -1)
+    {
+        grades.Add(grade);
+    }
+
+
+} while (grade != -1); // exit condition
+
 // Print values in list - for
 
+Console.WriteLine("Printing names via for loop");
+for (int i = 0; i < grades.Count; i++)
+{
+    Console.WriteLine(grades[i]);
+}
+
 // Print values in list - foreach
+Console.WriteLine("Printing names via foreach loop");
+foreach (int g in grades)
+{
+    Console.WriteLine(g);
+
+}
