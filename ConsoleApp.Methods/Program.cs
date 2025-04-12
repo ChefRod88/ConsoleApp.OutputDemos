e1e1e89bcdb5232f059e55b28e6b2cfb0dba6187
Owner <Owner@DESKTOP-BT3BM35>
2025-04-12 11:26:52 -0400

    Intial Commit

---
+++ ConsoleApp.Methods/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("********* - Methods - **********");
+
+// void methods - completes a task without returnng a value
+
+// value returning methods - returns a value after an operation
+
+// methods with parameters
+
+// methods with optional parameters
+
+// methods with nullable parameters
+
+/* Function Calls */
