c53ced3595c7ee8d081cdbfec39f562fe2955932
Owner <Owner@DESKTOP-BT3BM35>
2025-04-17 00:47:59 -0400

    Refactored User Input Pt. 2

--- ConsoleApp.UserInputDemo/Program.cs
+++ ConsoleApp.UserInputDemo/Program.cs
@@ -1,56 +1,81 @@
 ﻿// Declare variables
+using System.ComponentModel.Design;
 using System.Globalization;
+using System.Reflection;
+using System.Xml.Serialization;
 
-
+char userChoice = char.MinValue;
 string? firstName = string.Empty;
 string? lastName = string.Empty;
 int age;
 DateOnly dob = new DateOnly();
 decimal salary;
-char gender;
+char gender = char.MinValue;
 bool working;
 
-// Prompt the user for input
-Console.Write("Please enter your first name: ");
-firstName = Console.ReadLine();
+List<Person> persons = new List<Person>();
 
-Console.Write("Please enter your last name: ");
-lastName = Console.ReadLine();
+while (userChoice != 'E')
+{
+    Console.Write("Please enter your first name: ");
+    firstName = Console.ReadLine();
 
-Console.Write("Please enter your date of birth (dd/mm/yyyy) : ");
-dob = DateOnly.ParseExact(Console.ReadLine(), "dd/mm/yyyy",
-    CultureInfo.InvariantCulture);
+    Console.Write("Please enter your last name: ");
+    lastName = Console.ReadLine();
 
-
-Console.Write("Please enter your salary: ");
-salary = Convert.ToDecimal(Console.ReadLine());
-
-Console.Write("Please enter your gender (M or F): ");
-gender = Convert.ToChar(Console.ReadLine());
-
-Console.Write("Are you working? (true or false): ");
-working = Convert.ToBoolean(Console.ReadLine());
-
-Person person = new(dob) 
-{ 
-    FirstName = firstName,
-    LastName = lastName,
-    Gender = gender,
-    IsWorking = working,
-    Salary = salary
-};
-
-// Process the data 
-int workingYearsRemaining = person.GetNumberOfWorkingYearsRemaining();
-var estimatedRetirementDate = person.GetEstimatedRetirementDate();
+    Console.Write("Please enter your date of birth (dd/mm/yyyy) : ");
+    dob = DateOnly.ParseExact(Console.ReadLine(), "dd/mm/yyyy",
+        CultureInfo.InvariantCulture);
 
 
+    Console.Write("Please enter your salary: ");
+    salary = Convert.ToDecimal(Console.ReadLine());
 
-// Output the result
-Console.WriteLine($"Full name: {person.ToString()}");
-Console.WriteLine($"Age: {person.GetAge()}");
-Console.WriteLine($"Your Salary is: {person.Salary.ToString("C")}");
-Console.WriteLine($"Your Gender is: {person.Gender}");
-Console.WriteLine($"You are employed: {person.IsWorking}");
-Console.WriteLine($"Number of working years remaining: {workingYearsRemaining}");
-Console.WriteLine($"Estimated Retirement Year: {estimatedRetirementDate}");
+    Console.Write("Please enter your gender (M or F): ");
+    gender = Convert.ToChar(Console.ReadLine());
+
+    Console.Write("Are you working? (true or false): ");
+    working = Convert.ToBoolean(Console.ReadLine());
+
+    Person person = new(dob)
+    {
+        FirstName = firstName,
+        LastName = lastName,
+        Gender = gender,
+        IsWorking = working,
+        Salary = salary
+    };
+
+    persons.Add(person);
+
+    //persons.Add(new Person(dob)
+    //{
+    //    FirstName = firstName,
+    //    LastName = lastName,
+    //    Gender = gender,
+    //    IsWorking = working,
+    //    Salary = salary
+
+    //});
+
+    // Process the data 
+    Console.WriteLine("C - Continue | E - Exit");
+    userChoice = Convert.ToChar(Console.ReadLine());
+}
+
+foreach (var person in persons)
+{
+    // Output the results to the user
+    Console.WriteLine();
+    Console.WriteLine($"Full name: {person.ToString()}");
+    Console.WriteLine($"Age: {person.GetAge()}");
+    Console.WriteLine($"Your Salary is: {person.Salary.ToString("C")}");
+    Console.WriteLine($"Your Gender is: {person.Gender}");
+    Console.WriteLine($"You are employed: {person.IsWorking}");
+    Console.WriteLine($"Number of working years remaining: {person.GetNumberOfWorkingYearsRemaining()}");
+    Console.WriteLine($"Estimated Retirement Year: {person.GetEstimatedRetirementDate()}");
+
+}
+
+
+
